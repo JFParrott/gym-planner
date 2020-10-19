1bb556128c79813f7ce362e05fb0db36a49cd12a
JFParrott <jfparrott@protonmail.com>
2020-10-19 09:28:25 +0100

    start adding data

---
+++ db/development-data/workouts.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+    name: 'Pull Strength',
+    description: 'Deadlift, Bent over row and pull ups mandatory',
+  },
+  {
+    name: 'Push Strength',
+    description:
+      'Flat bench barbell press, overhead press and close grip bench mandatory',
+  },
+  {
+    name: 'Legs Strength',
+    description: 'Front squat, RDL and calf raise mandatory',
+  },
+  {
+    name: 'Pull HT',
+    description: 'Bent over row, seated row and lat pulldown mandatory',
+  },
+  {
+    name: 'Push HT',
+    description:
+      'Flat bench barbell press, seated shoulder press and dips mandatory',
+  },
+  {
+    name: 'Legs HT',
+    description: 'Squat, prone leg curl and leg press mandatory',
+  },
+];
---
+++ db/test-data/workouts.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'Fish',
+    description: 'Fishy',
+  },
+  {
+    name: 'Cakes',
+    description: 'Love them',
+  },
+  {
+    name: 'Rice',
+    description: 'and a rice cake',
+  },
+  {
+    name: 'Chicken',
+    description: 'dippers',
+  },
+  {
+    name: 'Broccoli',
+    description: 'is good',
+  },
+  {
+    name: 'Peas',
+    description: 'please',
+  },
+];
