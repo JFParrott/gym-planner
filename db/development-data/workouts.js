9657a55c3d5d548a37ed7f0d8f2b6ca6f61bb87d
JFParrott <jfparrott@protonmail.com>
2020-10-19 10:36:08 +0100

    add slugs to workout data

--- db/development-data/workouts.js
+++ db/development-data/workouts.js
@@ -2,27 +2,33 @@ module.exports = [
   {
     name: 'Pull Strength',
     description: 'Deadlift, Bent over row and pull ups mandatory',
+    slug: 'pull_strength',
   },
   {
     name: 'Push Strength',
     description:
       'Flat bench barbell press, overhead press and close grip bench mandatory',
+    slug: 'push_strength',
   },
   {
     name: 'Legs Strength',
     description: 'Front squat, RDL and calf raise mandatory',
+    slug: 'legs_strength',
   },
   {
     name: 'Pull HT',
     description: 'Bent over row, seated row and lat pulldown mandatory',
+    slug: 'pull_ht',
   },
   {
     name: 'Push HT',
     description:
       'Flat bench barbell press, seated shoulder press and dips mandatory',
+    slug: 'push_ht',
   },
   {
     name: 'Legs HT',
     description: 'Squat, prone leg curl and leg press mandatory',
+    slug: 'legs_ht',
   },
 ];
